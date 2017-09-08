b138c7d5fa16cbe9b7fabcb777a0d081d84a5cfb
Flávio Micheletti <flaviomicheletti@hotmail.com>
2017-09-08 15:24:24 -0300

    organizando

--- organizando-sua-app/objetivo.php
+++ organizando-sua-app/objetivo.php
@@ -5,7 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
-    <link rel="icon" href="../assets/favicon.ico">
 
     <title>Starter Template for Bootstrap</title>
 
@@ -13,7 +12,7 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
 
     <!-- Custom styles for this template -->
-    <link href="../assets/css/starter-template.css" rel="stylesheet">
+    <link href="starter-template.css" rel="stylesheet">
   </head>
 
   <body>
